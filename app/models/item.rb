b6024c6d1c6d58312d1e5f07510bff4119486773
RyousukeOkazaki <soratobuowan@gmail.com>
2021-08-27 16:41:25 +0900

    ActiveHashを実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,19 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :fee
+  belongs_to :prefecture
+  belongs_to :delivery_days
+
+  validates :name, :string, presence: true
+  validates :description, :text, presence: true
+  validates :price, :integer, presence: true
+
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :fee_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_days_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
