68c311e188c07d13c9d899b23c1705344d7358d0
RyousukeOkazaki <soratobuowan@gmail.com>
2021-09-02 17:15:58 +0900

    クレジット機能一部実装
    トークン取得まで

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,5 +3,23 @@ class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
     @fee = Fee.find(@item.fee_id)
+    @order = Order.new
+  end
+
+  def create
+    binding.pry
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private 
+
+  def order_params
+    params.require(:order)
   end
 end
