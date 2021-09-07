8251bf09e86555286d3a7becb7fd741ce2dd7a4d
RyousukeOkazaki <soratobuowan@gmail.com>
2021-09-07 12:45:15 +0900

    ウィザード形式実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :items
   has_many :orders
+  has_one :shipping_address
 
   validates :nickname, presence: true
   validates :password,
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    get 'shipping_addresses', to: 'users/registrations#new_shipping_address'
+    post 'shipping_addresses', to: 'users/registrations#create_shipping_address'
+  end
   root to: "items#index"
   resources :items do
     resources :orders, only: [:create, :index]
